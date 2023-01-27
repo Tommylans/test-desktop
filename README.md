06d8866f6eacda147c825a9382ab6f79bebf74ed
Tom Lanser <tom@devv.nl>
2023-01-27 16:24:16 +0100

    Added ci
    Signed-off-by: Tom Lanser <tom@devv.nl>

---
+++ README.md
@@ -0,0 +1,1 @@
+# Don't mind me I'm just testing something
--- forge.config.js
+++ forge.config.js
@@ -1,10 +1,34 @@
+const nodeenv = process.env.NODE_ENV
+const isProd = nodeenv === 'production'
+
+
 module.exports = {
-  packagerConfig: {},
+  packagerConfig: {
+    name: 'Test',
+    executableName: 'test-desktop',
+    appBundleId: 'nl.devv.test',
+    osxSign: isProd && {
+      identity: 'Developer ID Application: Tom Lanser (R74MU8J55F)',
+      hardenedRuntime: true,
+      entitlements: 'entitlements.mac.plist',
+      entitlementsInherit: 'entitlements.mac.plist',
+      gatekeeperAssess: false,
+    },
+    osxNotarize: isProd && {
+      appBundleId: 'nl.devv.test',
+      tool: 'notarytool',
+      appleApiKey: process.env.APPLE_API_KEY_PATH,
+      appleApiKeyId: process.env.APPLE_API_KEY_ID,
+      appleApiIssuer: process.env.APPLE_API_ISSUER,
+    },
+  },
   rebuildConfig: {},
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
-      config: {},
+      config: {
+        name: 'test-desktop',
+      },
     },
     {
       name: '@electron-forge/maker-zip',
@@ -12,11 +36,19 @@ module.exports = {
     },
     {
       name: '@electron-forge/maker-deb',
-      config: {},
+      config: {
+        options: {
+          bin: 'test-desktop',
+        },
+      },
     },
     {
       name: '@electron-forge/maker-rpm',
-      config: {},
+      config: {
+        options: {
+          bin: 'test-desktop',
+        },
+      },
     },
   ],
 };
